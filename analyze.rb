a556a780084dcec4ea5903b63f98075e7c6288d1
Kevin McHugh <kev@kevinmchugh.me>
2017-09-11 14:27:06 -0500

    updates

--- analyze.rb
+++ analyze.rb
@@ -1,12 +1,17 @@
+require 'csv'
+require 'pry'
+
 rows = CSV.parse(File.read('augmented_fighters.csv'), headers: true)
 by_height = rows.group_by { |r| r['height'] }
 by_height.delete(nil)
-by_height.keep_if { |k,v| v.any?}.map do |k, vals|
-  sum = vals.keep_if { |r| r['reach'] }.map { |r| r['reach'].to_f }.reduce(:+) || 0
-  { height: k, avg_reach: sum/vals.count }
-end
 avg_reach_by_height = by_height.keep_if { |k,v| v.any?}.map do |k, vals|
   sum = vals.keep_if { |r| r['reach'] }.map { |r| r['reach'].to_f }.reduce(:+) || 0
-  { height: k, avg_reach: sum/vals.count }
-end
+  if vals.any?
+    { height: k, avg_reach: sum/vals.count || 1 }
+  else
+    nil
+  end
+end.compact
 avg_reach_by_height.map { |h| {h[:height] => h[:avg_reach] / h[:height].to_i } }
+# binding.pry
+puts by_height.values.flatten.find_all {|v| v['reach_to_height'].to_f > 1.1 }.map { |v| v['name'] }
